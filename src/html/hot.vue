9d460a506c9926588897e919ed3e01bf784798c1
Jack.Wang <jack.wang@integem.com>
2018-01-12 00:29:02 +0800

    init

--- src/html/hot.vue
+++ src/html/hot.vue
@@ -1,26 +1,14 @@
 <template>
 	<div class="main">
-		<ul class="items">
-			<b-media tag="li" v-for="item of items" :key="item._id" @click="$router.push({path:`/${item._id}`})">
-				<b-img slot="aside" :src="item.pict_url" width="100" height="100" :alt="item.title" />
-				<h5 class="mt-0" v-text="item.title"></h5>
-				<p>
-					<span>现价:{{item.zk_final_price}} 原价:{{item.reserve_price}}</span>
-					<span>销量:{{item.volume}}</span>
-					<br/>
-				</p>
-				<p>
-					<span>@{{item.shop_title}}</span>
-					<span>{{item.provcity}}</span>
-					<br/>
-				</p>
-			</b-media>
-		</ul>
+		<vue-list :items="items"></vue-list>
 	</div>
 </template>
 
 <script>
+	import VueList from "./include/vueList.vue";
+
 	export default {
+		components: {VueList},
 		name: "Hot",
 		data: function () {
 			return {
@@ -33,28 +21,32 @@
 			}
 		},
 		mounted: function () {
-			this.$db.table("items").where({
-				click_url: {
-					$ne: null
+			this.$db.table("items").where(
+				{
+					click_url: {
+						$ne: null
+					}
 				}
-			}).other({
-				limit: 20,
-				skip: 0,
-				show: {
-					_id: 1,
-					title: 1,
-					shop_title: 1,
-					zk_final_price: 1,
-					reserve_price: 1,
-					provcity: 1,
-					volume: 1,
-					pict_url: 1
+			).other(
+				{
+					limit: 20,
+					skip: 0,
+					show: {
+						_id: 1,
+						title: 1,
+						shop_title: 1,
+						zk_final_price: 1,
+						reserve_price: 1,
+						provcity: 1,
+						volume: 1,
+						pict_url: 1
+					}
 				}
-			}).get("find").then(items => {
+			).get("find").then(items => {
 				this.items = items;
 			});
 			this.$nextTick(() => {
-				this.$store.commit('foot', !this.$route.meta.hideFooter);
+				this.$store.commit("foot", !this.$route.meta.hideFooter);
 				this.$store.commit("title", `推荐 | ${this.$store.state.site.title}`);
 			});
 		}
@@ -63,30 +55,6 @@
 </script>
 
 <style>
-	ul.items>li {
-		margin: 0;
-		padding: 10px;
-		cursor: pointer;
-	}
 
-	ul.items>li:hover {
-		background: #92baff;
-	}
-
-	ul.items>li>div.media-body>p {
-		margin-bottom: 0;
-	}
-
-	ul.items>li>div.media-body>p>br {
-		clear: both;
-	}
-
-	ul.items>li>div.media-body>p>span {
-		float: right;
-	}
-
-	ul.items>li>div.media-body>p>span:first-child {
-		float: left;
-	}
 
 </style>
---
+++ src/html/include/vueList.vue
@@ -0,0 +1,48 @@
+<template>
+	<pull-to :top-load-method="refresh">
+		<ul class="items">
+			<b-media tag="li" v-for="item of items" :key="item._id" @click="$router.push({path:`/${item._id}`})">
+				<b-img slot="aside" :src="item.pict_url" width="100" height="100" :alt="item.title"/>
+				<h5 class="mt-0" v-text="item.title"></h5>
+				<p>
+					<span>现价:{{item.zk_final_price}} 原价:{{item.reserve_price}}</span>
+					<span>销量:{{item.volume}}</span>
+					<br/>
+				</p>
+				<p>
+					<span>@{{item.shop_title}}</span>
+					<span>{{item.provcity}}</span>
+					<br/>
+				</p>
+			</b-media>
+		</ul>
+	</pull-to>
+</template>
+
+<script>
+	import "../css/list.css";
+	import PullTo from "vue-pull-to";
+
+	export default {
+		name: "vue-list",
+		components: {
+			PullTo
+		},
+		props: ["items"],
+		mounted: function () {
+
+		},
+		methods: {
+			refresh: function(loaded) {
+				setTimeout(() => {
+					// this.dataList.reverse();
+					loaded('done');
+				}, 5000);
+			}
+		}
+	};
+</script>
+
+<style>
+
+</style>
