117fce9f3a616b63609fc65dc262462fe750ccf5
Jack.Wang <jack.wang@integem.com>
2018-01-17 00:37:16 +0800

    init

--- src/html/item.vue
+++ src/html/item.vue
@@ -16,59 +16,57 @@
 </template>
 
 <script>
-	export default {
-		name: "Item",
-		data: function () {
-			return {
-				item: {}
-			};
-		},
-		mounted: function () {
-			this.$nextTick(() => {
-				this.$store.commit("foot", !this.$route.meta.hideFooter);
-				this.$db
-					.table("items")
-					.where(
-						{
-							_id: this.$route.params.id
-						}
-					)
-					.get("findone")
-					.then(item => {
-						this.item = item;
-						this.$store.commit(
-							"title",
-							`${item.title} | ${this.$store.state.site.title}`
-						);
-					});
-			});
-		}
-	};
+export default {
+	name: "Item",
+	data: function() {
+		return {
+			item: {}
+		};
+	},
+	mounted: function() {
+		this.$nextTick(() => {
+			this.$store.commit("foot", !this.$route.meta.hideFooter);
+			this.$db
+				.table("items")
+				.where({
+					_id: this.$route.params.id
+				})
+				.get("findone")
+				.then(item => {
+					this.item = item;
+					this.$store.commit(
+						"title",
+						`${item.title} | ${this.$store.state.site.title}`
+					);
+				});
+		});
+	}
+};
 </script>
 
 <style>
-	div.images > ul {
-		position: relative;
-		margin: 0;
-		padding: 0;
-	}
+div.images > ul {
+	position: relative;
+	margin: 0;
+	padding: 0;
+}
 
-	div.images > ul > li {
-		margin: 0;
-		width: 100%;
-		overflow: hidden;
-		height: 0;
-		padding-bottom: 100%;
-		display: flex;
-		display: -webkit-flex;
-		align-items: center;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
+div.images > ul > li {
+	margin: 0;
+	width: 100%;
+	overflow: hidden;
+	height: 0;
+	padding-bottom: 100%;
+	display: flex;
+	display: -webkit-flex;
+	align-items: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
 
-	div.images > ul > li > img {
-		width: 100%;
-		padding-top: 100%;
-	}
+div.images > ul > li > img {
+	width: 100%;
+	padding-top: 100%;
+}
 </style>
