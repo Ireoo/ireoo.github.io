19a3719f1b4e7c4a42492d8e2329a596359f19e9
Jack Wang <jack.wang@integem.com>
2018-04-11 19:45:08 +0800

    添加自动获取插件更新

--- src/route.js
+++ src/route.js
@@ -5,50 +5,51 @@ Vue.use(Router);
 
 export default new Router({
 	// mode: "history",
-	routes: [{
+	routes: [
+		{
 			path: "/",
 			name: "Index",
-			component: (resolve) => require(["./html/index.vue"], resolve)
+			component: resolve => require(["./html/index.vue"], resolve)
 		},
 		{
 			path: "/list",
 			name: "List",
-			component: (resolve) => require(["./html/list.vue"], resolve)
+			component: resolve => require(["./html/list.vue"], resolve)
 		},
 		{
 			path: "/login",
 			name: "Login",
-			component: (resolve) => require(["./html/login.vue"], resolve)
+			component: resolve => require(["./html/login.vue"], resolve)
 		},
 		{
 			path: "/reg",
 			name: "Reg",
-			component: (resolve) => require(["./html/reg.vue"], resolve)
+			component: resolve => require(["./html/reg.vue"], resolve)
 		},
 		{
 			path: "/user",
 			name: "User",
-			component: (resolve) => require(["./html/user.vue"], resolve)
+			component: resolve => require(["./html/user.vue"], resolve)
 		},
 		{
 			path: "/hot",
 			name: "Hot",
-			component: (resolve) => require(["./html/hot.vue"], resolve)
+			component: resolve => require(["./html/hot.vue"], resolve)
 		},
 		{
 			path: "/cart",
 			name: "Cart",
-			component: (resolve) => require(["./html/cart.vue"], resolve)
+			component: resolve => require(["./html/cart.vue"], resolve)
 		},
 		{
 			path: "/search",
 			name: "Search",
-			component: (resolve) => require(["./html/search.vue"], resolve)
+			component: resolve => require(["./html/search.vue"], resolve)
 		},
 		{
 			path: "/:id",
 			name: "Item",
-			component: (resolve) => require(["./html/item.vue"], resolve),
+			component: resolve => require(["./html/item.vue"], resolve),
 			meta: {
 				hideFooter: true
 			}
